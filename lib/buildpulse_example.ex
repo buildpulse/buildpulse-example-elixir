76d11ca764debd519518d4b5ac703f28a9ef5949
Sid Dange <dange.siddhant@gmail.com>
2023-03-15 16:29:27 -0700

    initial commit

---
+++ lib/buildpulse_example.ex
@@ -0,0 +1,18 @@
+defmodule BuildpulseExample do
+  @moduledoc """
+  Documentation for `BuildpulseExample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BuildpulseExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/buildpulse_example_test.exs
@@ -0,0 +1,8 @@
+defmodule BuildpulseExampleTest do
+  use ExUnit.Case
+  doctest BuildpulseExample
+
+  test "greets the world" do
+    assert 1 + 1 == 2
+  end
+end
